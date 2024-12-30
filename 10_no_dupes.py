7dc9a411de8c38cd93614dedd11a12c7c6c0828c
unknown <gianenabore30@gmail.com>
2024-12-31 03:45:28 +0800

    ask the user if they want to input another entry

--- 10_no_dupes.py
+++ 10_no_dupes.py
@@ -1,16 +1,21 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 numbers = []
+while True:
+    
+    for i in range(10):
+        while True:
+            try:
+                num = int(input("please enter a number: "))
+                numbers.append(num)
+                break
+            except ValueError:
+                print("Invalid input. please enter a interger only.")
 
-for i in range(10):
-    while True:
-        try:
-            num = int(input("please enter a number: "))
-            numbers.append(num)
-            break
-        except ValueError:
-            print("Invalid input. please enter a interger only.")
+    no_dupes = [num for num in numbers if numbers.count(num) == 1]
+    print(f"the numbers without duplicates is {no_dupes}")
 
-no_dupes = [num for num in numbers if numbers.count(num) == 1]
-print(f"the numbers without duplicates is {no_dupes}")
-
+    retry = input("do you want to try again? y/n: ").strip().lower()
+    if retry != "y":
+        print("THANK YOU!!")
+        break

ba7983905939519c9b02a1c8c232bc238073cb2e
unknown <gianenabore30@gmail.com>
2024-12-31 03:42:16 +0800

    determine the numbers without duplicates and output them as unique numbers

--- 10_no_dupes.py
+++ 10_no_dupes.py
@@ -6,6 +6,11 @@ for i in range(10):
     while True:
         try:
             num = int(input("please enter a number: "))
-        
+            numbers.append(num)
+            break
         except ValueError:
             print("Invalid input. please enter a interger only.")
+
+no_dupes = [num for num in numbers if numbers.count(num) == 1]
+print(f"the numbers without duplicates is {no_dupes}")
+

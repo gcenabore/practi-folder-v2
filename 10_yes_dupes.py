6c828515d6d8f1257addd01a79c4c778c8e52e7a
unknown <gianenabore30@gmail.com>
2024-12-31 19:26:05 +0800

    initial commit

--- 10_yes_dupes.py
+++ 10_yes_dupes.py
@@ -1,11 +1,22 @@
 
 number = []
-
-for i in range(10):
-    try:
-        num = int(input("Please enter 10 different numbers: "))
-        number.append(num)
+while True:
+    for i in range(10):
+        while True:
+            try:
+                num = int(input("Please enter 10 different numbers: "))
+                number.append(num)
+                break
+            except ValueError:
+                print("ERROR: please enter a integer value only..")
+            
+    duplicates = list(set([num for num in number if number.count(num) > 1]))
+    print(f"the numbers with duplicates are {duplicates}.")
+    
+    retry = input("Do you want to try again? y/n: ").strip().lower()
+    if retry != "y":
+        print("THANK YOU!!")
         break
-    except ValueError:
-        print("ERROR: please enter a integer value only..")
-        
+
+
+
